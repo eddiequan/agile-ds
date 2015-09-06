7229a34bc50fa96bb6fbeefe4bed610d4a9ac114
Eddie Quan <eddieq@nulogy.com>
2015-09-06 12:31:08 -0400

    WIP

---
+++ app/controllers/iterations_controller.rb
@@ -0,0 +1,23 @@
+class IterationsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @iteration = Iteration.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
